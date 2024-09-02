7da5a9ecd6f342bf4e60fa59ce390b3ec9fc7ac8
wfy3364 <2489589935@qq.com>
2024-09-03 02:03:05 +0800

    UserInfo, AdminDeviceDetail Updated

--- src/views/AdminDeviceDetail.vue
+++ src/views/AdminDeviceDetail.vue
@@ -1,23 +1,49 @@
 <script setup>
-import { ref } from 'vue';
+import { ref, computed } from 'vue';
 import { useRouter } from 'vue-router'; // 导入 useRouter
 import { convertTime, judgeState } from '@/apis/utils';
+import RepairDetail from './components/RepairDetail.vue';
+import { useUserStore } from '@/stores/userStore';
+import { storeToRefs } from 'pinia';
 
 const router = useRouter(); // 初始化路由器
+const curRecord = ref(null);
+const detailDialog = ref(false);
+const dialogMode = ref('view');
+const userStore = useUserStore();
+const { adminType, adminPermission } = storeToRefs(userStore);
+const deviceEdit = ref(false);
 
-const deviceInfo = {
+function showRepairDetail(record, mode){
+  curRecord.value = record;
+  dialogMode.value = mode;
+  detailDialog.value = true;
+}
+
+const EditCheck = computed(() => { //检查管理员权限
+  return adminType.value === 'system' ||
+        (adminType.value === 'device' || adminType.value === 'venue-device') &&
+        record.deviceId in adminPermission.value.device
+});
+
+const deviceInfo = ref({
   id: 1,
   name: "设备01",
   venue: "场地1",
   state: 0,
   introTime: "2024-08-20 13:00"
-};
+});
 
-const overviewDisplay = {
-  "设备ID：": deviceInfo.id,
-  "设备名称：": deviceInfo.name,
-  "设备引入时间：": deviceInfo.introTime, 
-};
+const tempDeviceInfo = ref({
+  name: deviceInfo.value.name,
+  venue: deviceInfo.value.venue,
+})
+
+// const overviewDisplay = ref({
+//   "设备ID：": deviceInfo.value.id,
+//   "设备名称：": deviceInfo.value.name,
+//   "设备引入时间：": deviceInfo.value.introTime, 
+// });
 
 // 计算维修记录状态
 const repairRecord = [{
@@ -67,6 +93,25 @@ repairRecord.map((record) => {
   return record;
 })
 
+function showDeviceEdit() {
+  deviceEdit.value = true;
+}
+
+function closeDeviceEdit() {
+  deviceEdit.value = false;
+}
+
+function saveDeviceEdit() {
+  deviceInfo.value.name=tempDeviceInfo.value.name;
+  deviceInfo.value.venue=tempDeviceInfo.value.venue;
+  console.log(tempDeviceInfo.value.name);
+  console.log(deviceInfo.value.name);
+  ElMessage({
+    message: '信息修改成功',
+    type: 'success',
+  })
+  deviceEdit.value = false;
+}
 
 // 跳转到设备列表页面
 function goToDeviceList() {
@@ -79,14 +124,26 @@ function goToDeviceList() {
     <div class="AdminDeviceHeader">
       <el-button class="BackButton" @click="goToDeviceList">&lt;&nbsp;&nbsp;设备总览</el-button>
       <div class="AdminDeviceTitle">{{ deviceInfo.name }}</div>
-      <el-button class="EditButton">编辑</el-button>
+      <el-button class="EditButton" @click="showDeviceEdit">编辑</el-button>
     </div>
     <div class="DeviceOverview">
       <img class="DeviceImg" alt="设备图片"/>
       <div class="OverviewText">
-        <div class="OverviewLine" v-for="(val, k) in overviewDisplay" :key="k">
+        <!-- <div class="OverviewLine" v-for="(val, k) in overviewDisplay" :key="k">
           <div class="OverviewDescription">{{ k }}</div>
           <div>{{ val }}</div>
+        </div> -->
+        <div class="OverviewLine">
+          <div class="OverviewDescription">设备ID：</div>
+          <div>{{ deviceInfo.id }}</div>
+        </div>
+        <div class="OverviewLine">
+          <div class="OverviewDescription">设备名称：</div>
+          <div>{{ deviceInfo.name }}</div>
+        </div>
+        <div class="OverviewLine">
+          <div class="OverviewDescription">设备引入时间：</div>
+          <div>{{ deviceInfo.introTime }}</div>
         </div>
         <div class="OverviewLine">
           <div class="OverviewDescription">设备所在场地：</div>
@@ -115,12 +172,52 @@ function goToDeviceList() {
               <div v-if="item.row.state === 2" style="color: orange">待维修</div>
             </template>
           </el-table-column>
-          <el-table-column label="操作" width="100">
+          <el-table-column label="操作" width="150">
             <template #default="item">
-              <el-button @click="showDetails(item.row)">详情</el-button>
+              <el-button size="small" type="primary" @click="showRepairDetail(item.row, 'view')">详情</el-button>
+              <el-button size="small" v-if="EditCheck" @click="showRepairDetail(item.row, 'edit')">编辑</el-button>
             </template>
           </el-table-column>
         </el-table>
+      </div>
+    </div>
+  </div>
+  <RepairDetail v-if="detailDialog" :dialogMode="dialogMode" :curRecord="curRecord" 
+  @closeModal="detailDialog = false" @editModal="showRepairDetail(curRecord, 'edit')"></RepairDetail>
+  <!-- <div v-if="deviceEdit" class="modal" @click="closeDeviceEdit">
+    <div class="modal-content2" @click.stop>
+      <div class="modalHeader">
+        <div class="modalTitle">设备信息修改</div>
+      </div>
+      <div class="smallButtonContainer"> 
+        <el-button class="smallButton" type="primary" @click="saveDeviceEdit">保存</el-button>
+        <el-button class="smallButton" @click="closeDeviceEdit">取消</el-button>
+      </div>
+    </div>
+  </div> -->
+  <div v-if="deviceEdit" class="modal" @click="closeDeviceEdit">
+    <div class="modal-content" @click.stop>
+      <div class="modalHeader">
+        <div class="modalTitle">编辑信息</div>
+        <el-button class="smallButton" type="danger" @click="closeDeviceEdit">关闭</el-button>
+      </div>
+      <div class="modalBody">
+        <el-form :model="tempDeviceInfo" ref="form" label-width="120px">
+          <div class="form-row">
+            <el-form-item class="form-item" label="设备名" prop="name">
+              <el-input v-model="tempDeviceInfo.name" placeholder=""></el-input>
+            </el-form-item>
+          </div>
+          <div class="form-row">
+            <el-form-item class="form-item" label="场地" prop="venue">
+              <el-input v-model="tempDeviceInfo.venue" placeholder=""></el-input>
+            </el-form-item>
+          </div>
+          <div class="smallButtonContainer"> 
+            <el-button class="smallButton" @click="closeDeviceEdit">取消</el-button>
+            <el-button class="smallButton" type="primary" @click="saveDeviceEdit">保存</el-button>
+          </div>
+        </el-form>
       </div>
     </div>
   </div>
@@ -211,4 +308,60 @@ function goToDeviceList() {
   border: 1px solid lightgray;
 }
 
+.modal {
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(0, 0, 0, 0.6); /* 半透明背景 */
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  z-index: 1000;
+}
+
+.modal-content {
+  width: 60%;
+  max-width: 600px;
+  background-color: #fff;
+  padding: 20px;
+  border-radius: 8px;
+  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.2);
+  position: relative;
+}
+
+.modalHeader {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-bottom: 20px;
+}
+
+.modalTitle {
+  font-size: 22px;
+  font-weight: bold;
+}
+
+.modalBody {
+  margin-top: 20px;
+}
+
+.smallButtonContainer {
+  display: flex;
+  justify-content: flex-end;
+  /* margin-right: auto; */
+}
+
+.smallButton {
+  /* margin-right: auto; */
+  margin-top: 10px;
+}
+
+.form-row {
+  display: flex;
+  align-items: center;
+  margin-bottom: 10px;
+  
+}
 </style>
